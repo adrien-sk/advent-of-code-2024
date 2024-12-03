39e450d49e3213fa5346ee959543559a8b28af2d
adrien-sk <skrzypczak.adrien@gmail.com>
2024-12-03 22:07:19 +0100

    Day 3 part 1

---
+++ day_3/part_1.py
@@ -0,0 +1,16 @@
+import re
+
+# Open file and regex to find all string like : mul(123,123)
+file = open('input.txt', 'r').read()
+pattern = r"mul\(\d{1,3},\d{1,3}\)"
+mul_list = re.findall(pattern, file)
+res = 0
+
+# For each string, regex to get the 2 numbers using Group, and multiply + sum to the result
+for mul in mul_list:
+    pattern_groups = r"mul\((\d{1,3}),(\d{1,3})\)"
+    test = re.match(pattern_groups, mul)
+    num1, num2 = test.groups()
+    res += int(num1) * int(num2)
+
+print(res)
